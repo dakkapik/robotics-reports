5015ad14939e78f3988e835391c7a7896ad737a3
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-05-11 19:32:31 -0400

    heroku db

--- setup/db.js
+++ setup/db.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// Connect MongoDB at default port 27017.
-mongoose.connect('mongodb://localhost:27017/MDC-robotics', {
+let url
+switch(process.env.NODE_ENV){
+    case "development" : url = 'mongodb://localhost:27017/MDC-robotics';
+    case "production" : url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dab6a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+}
+
+mongoose.connect(url, {
     useNewUrlParser: true
 }, (err) => {
     if (!err) {
-        console.log('> MongoDB Connection Succeeded.')
+        console.log('> MongoDB Connection Succeeded on ' + process.env.NODE_ENV + ' mode.')
+
     } else {
         console.log('Error in DB connection: ' + err)
     }
